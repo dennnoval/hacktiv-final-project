4f0be13f63effbb8a1fefb56920682038cb8e08a
dennnoval <dennnoval@gmail.com>
2022-10-24 22:52:44 +0700

    Create socialMedia.go

---
+++ entity/socialMedia.go
@@ -0,0 +1,16 @@
+package entity
+
+import (
+	t "time"
+)
+
+type SocialMedia struct {
+	SocialMediaID int `gorm:"primaryKey;column:id;autoIncrement" json:"id"`
+	CreatedAt t.Time `json:"createdAt"`
+  UpdatedAt t.Time `json:"updatedAt"`
+	Name string `json:"name"`
+	SocialMediaUrl string `json:"social_media_url"`
+	UserID int `gorm:"not null" json:"UserId"`
+	User User `gorm:"references:UserID" json:"-"`
+	Photo Photo `gorm:"foreignKey:PhotoID" json="-"`
+}
