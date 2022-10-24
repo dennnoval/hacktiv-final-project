6efa9f52e4cb805f6b5929b9872e2a413cffb2cc
dennnoval <dennnoval@gmail.com>
2022-10-24 22:52:29 +0700

    Create commentC.go

---
+++ controller/commentC.go
@@ -0,0 +1,18 @@
+package controller
+
+import (
+	// e "hacktiv-final-project/entity"
+	g "github.com/gin-gonic/gin"
+	// h "net/http"
+	// "gorm.io/gorm"
+	// s "strconv"
+	// b "golang.org/x/crypto/bcrypt"
+)
+
+func GetAllComments(ctx *g.Context) {}
+
+func AddComment(ctx *g.Context) {}
+
+func UpdateComment(ctx *g.Context) {}
+
+func DeleteComment(ctx *g.Context) {}
