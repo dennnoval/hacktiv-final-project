9cf546b2ddb6052d23a39a40b351b69a103485c4
dennnoval <dennnoval@gmail.com>
2022-10-24 22:52:32 +0700

    Create photoC.go

---
+++ controller/photoC.go
@@ -0,0 +1,18 @@
+package controller
+
+import (
+	// e "hacktiv-final-project/entity"
+	g "github.com/gin-gonic/gin"
+	// h "net/http"
+	// "gorm.io/gorm"
+	// s "strconv"
+	// b "golang.org/x/crypto/bcrypt"
+)
+
+func GetAllPhotos(ctx *g.Context) {}
+
+func AddPhoto(ctx *g.Context) {}
+
+func UpdatePhoto(ctx *g.Context) {}
+
+func DeletePhoto(ctx *g.Context) {}
