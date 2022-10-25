2e836bf96f662ddafeccd99bda4a656d08c95a21
dennnoval <dennnoval@gmail.com>
2022-10-25 19:26:39 +0700

    Update comment.go

--- entity/comment.go
+++ entity/comment.go
@@ -8,7 +8,7 @@ type Comment struct {
 	CommentID int `gorm:"primaryKey;column:id;autoIncrement"`
 	CreatedAt t.Time
   UpdatedAt t.Time
-  Message string `validate:"required"`
-	UserID int `gorm:"not null"`
-	PhotoID int `gorm:"not null"`
+  Message string `validate:"required" json:"message"`
+	UserID int `gorm:"not null" json:"user_id"`
+	PhotoID int `gorm:"not null" json:"photo_id"`
 }
