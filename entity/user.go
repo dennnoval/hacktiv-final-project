2aa64d23fa7aa0511019a8a3e6f216177e6fc5d6
LAPTOP-P170T266\noval <dennnoval@gmail.com>
2022-10-25 08:26:42 +0700

    Update user.go

--- entity/user.go
+++ entity/user.go
@@ -5,14 +5,14 @@ import (
 )
 
 type User struct {
-	UserID int `gorm:"primaryKey;column:id;autoIncrement"`
-	CreatedAt t.Time
-  UpdatedAt t.Time
-  Username string `gorm:"uniqueIndex"`
-	Email string `gorm:"uniqueIndex"`
-	Password string
-	Age int
-	Photo []Photo `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE" validate:"-"`
-	// SocialMedia SocialMedia `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE" validate:"-"`
-	Comment []Comment `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE" validate:"-"`
+	UserID      int `gorm:"primaryKey;column:id;autoIncrement"`
+	CreatedAt   t.Time
+	UpdatedAt   t.Time
+	Username    string `gorm:"uniqueIndex"`
+	Email       string `gorm:"uniqueIndex"`
+	Password    string
+	Age         int
+	Photo       []Photo     `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE" validate:"-"`
+	SocialMedia SocialMedia `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE" validate:"-"`
+	Comment     []Comment   `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE" validate:"-"`
 }
