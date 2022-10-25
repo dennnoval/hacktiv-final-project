589f781a783901de79544185aa0dde554a94172e
LAPTOP-P170T266\noval <dennnoval@gmail.com>
2022-10-25 15:06:52 +0700

    Update photoC.go

--- controller/photoC.go
+++ controller/photoC.go
@@ -5,13 +5,38 @@ import (
 
 	g "github.com/gin-gonic/gin"
 
-	// h "net/http"
+	s "strconv"
+
 	"gorm.io/gorm"
-	// s "strconv"
-	// b "golang.org/x/crypto/bcrypt"
 )
 
-func GetAllPhotos(ctx *g.Context) {}
+func GetAllPhotos(ctx *g.Context) {
+	ps := []e.Photo{}
+	db := ctx.MustGet("db").(*gorm.DB)
+	err := db.Find(&ps).Error
+	if err != nil {
+		ctx.AbortWithStatusJSON(200, err)
+	} else {
+		psms := make([]interface{}, len(ps))
+		for i, v := range ps {
+			psm := map[string]interface{}{}
+			psm["id"] = v.PhotoID
+			psm["title"] = v.Title
+			psm["caption"] = v.PhotoUrl
+			psm["user_id"] = v.UserID
+			psm["created_at"] = v.CreatedAt
+			psm["updated_at"] = v.UpdatedAt
+			u := e.User{UserID: v.UserID}
+			db.First(&u)
+			um := map[string]interface{}{
+				"email": u.Email, "username": u.Username,
+			}
+			psm["User"] = um
+			psms[i] = psm
+		}
+		ctx.JSON(200, psms)
+	}
+}
 
 func AddPhoto(ctx *g.Context) {
 	p := ctx.MustGet("photo").(e.Photo)
@@ -29,6 +54,29 @@ func AddPhoto(ctx *g.Context) {
 	}
 }
 
-func UpdatePhoto(ctx *g.Context) {}
+func UpdatePhoto(ctx *g.Context) {
+	p := ctx.MustGet("photo").(e.Photo)
+	pId, _ := s.Atoi(ctx.Param("photoId"))
+	p.PhotoID = pId
+	db := ctx.MustGet("db").(*gorm.DB)
+	if err := db.Model(&p).Where("id = ?", p.PhotoID).Updates(&p).Error; err != nil {
+		ctx.AbortWithStatusJSON(200, g.H{"message": err.Error()})
+	} else {
+		db.Take(&p)
+		ctx.JSON(200, g.H{
+			"id": p.PhotoID, "title": p.Title, "caption": p.Caption,
+			"photo_url": p.PhotoUrl, "user_id": p.UserID, "updated_at": p.CreatedAt,
+		})
+	}
+}
 
-func DeletePhoto(ctx *g.Context) {}
+func DeletePhoto(ctx *g.Context) {
+	pId, _ := s.Atoi(ctx.Param("photoId"))
+	p := e.Photo{PhotoID: pId}
+	db := ctx.MustGet("db").(*gorm.DB)
+	if err := db.Delete(&p).Error; err != nil {
+		ctx.AbortWithStatusJSON(200, g.H{"message": "Social media doesn't exists"})
+		return
+	}
+	ctx.JSON(200, g.H{"message": "Your photo has been deleted successfully"})
+}
